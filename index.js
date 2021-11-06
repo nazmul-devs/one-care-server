13673f530e424e8782e9cd9be687de025c8e909c
Nazmul Hosen <nazmulhosenm668@gmail.com>
2021-11-06 17:51:17 +0600

    connect server with mongoDb database

--- index.js
+++ index.js
@@ -2,9 +2,20 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 require("dotenv").config();
+const { MongoClient } = require("mongodb");
+
 const port = 5000;
 app.use(cors());
 app.use(express.json());
+
+// mongodb connection
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.f4mgp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+console.log(uri);
+const client = new MongoClient(uri, {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+});
 
 // get app
 app.get("/", (req, res) => {
