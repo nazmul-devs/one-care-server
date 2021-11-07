45a3c63a03e34a730275955b1eeea4f81334b737
Nazmul Hosen <nazmulhosenm668@gmail.com>
2021-11-08 02:02:17 +0600

    get data by secure with jwt token

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 const express = require("express");
 const cors = require("cors");
+const admin = require("firebase-admin");
 const app = express();
 require("dotenv").config();
 const { MongoClient } = require("mongodb");
+
+// firebase idtoken
+const serviceAccount = require("./one-care-family-doctors.json");
+
+admin.initializeApp({
+	credential: admin.credential.cert(serviceAccount),
+});
 
 const port = 5000;
 app.use(cors());
@@ -16,6 +24,18 @@ const client = new MongoClient(uri, {
 	useUnifiedTopology: true,
 });
 
+// token varify
+async function varifyToken(req, res, next) {
+	if (req.headers?.authorization?.startsWith("Bearer ")) {
+		const token = req.headers.authorization.split(" ")[1];
+
+		try {
+			const decodetUser = await admin.auth().verifyIdToken(token);
+			req.decodetEmail = decodetUser.email;
+		} catch {}
+	}
+	next();
+}
 async function run() {
 	try {
 		await client.connect();
@@ -31,7 +51,7 @@ async function run() {
 		});
 
 		// get appoinment data from database by email and date
-		app.get("/appoinment", async (req, res) => {
+		app.get("/appoinment", varifyToken, async (req, res) => {
 			const email = req.query.email;
 			const date = req.query.date;
 			const result = await apoinmentCollection
@@ -60,13 +80,35 @@ async function run() {
 			res.json(result);
 		});
 		// set admin
-		app.put("/users/admin", async (req, res) => {
+		app.put("/users/admin", varifyToken, async (req, res) => {
 			const user = req.body;
-			const filter = { email: user.email };
-			const updateDoc = { $set: { role: "admin" } };
-			const result = await usersCollection.updateOne(filter, updateDoc);
-			res.json(result);
-			console.log(result);
+			const requister = req.decodetEmail;
+			if (requister) {
+				const requisterAccount = await usersCollection.findOne({
+					email: requister,
+				});
+				if (requisterAccount.role === "admin") {
+					const filter = { email: user.email };
+					const updateDoc = { $set: { role: "admin" } };
+					const result = await usersCollection.updateOne(
+						filter,
+						updateDoc
+					);
+					res.json(result);
+				} else {
+					res.status(403).json({ message: "you do not have access" });
+				}
+			}
+		});
+		// check admin rule
+		app.get("/users/:email", async (req, res) => {
+			const userEmail = req.params.email;
+			const result = await usersCollection.findOne({ email: userEmail });
+			let isAdmin = false;
+			if (result?.role === "admin") {
+				isAdmin = true;
+			}
+			res.json({ admin: isAdmin });
 		});
 	} finally {
 		// await client.close();
